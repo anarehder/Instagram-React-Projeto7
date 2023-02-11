497a79b9cc622cc6e533ea34de64c1c7b6d2521f
anarehder <anapaula.rehder@gmail.com>
2023-02-11 17:57:46 -0300

    Alterar Nome e Imagem Usuario

--- src/components/Usuario.js
+++ src/components/Usuario.js
@@ -1,11 +1,38 @@
+import { useState } from 'react'
+
 export default function Usuario() {
+  const nomeUsuarioInicial = "catanacomics";
+  const fotoUsuarioInicial = "assets/img/catanacomics.svg";
+
+  return (
+    <AlteraNomeFoto nomeUsuario = {nomeUsuarioInicial} fotoUsuario = {fotoUsuarioInicial}/>
+  );
+}
+
+function AlteraNomeFoto (props) {
+  // inicializando com o nome de usuario e fotos iniciais
+    const [nomeUsuario, setNomeUsuario] = useState(props.nomeUsuario);
+    const [fotoUsuario, setFotoUsuario] = useState(props.fotoUsuario);
+
+    function alteraNome(){
+        const novoNomeUsuario = prompt("Digite seu nome de usuário:");
+        const comprimentoNome = novoNomeUsuario.trim().length;
+        setNomeUsuario(comprimentoNome === 0 ? nomeUsuario : novoNomeUsuario);
+    }
+  
+    function alteraFoto(){
+        const novaFotoUsuario = prompt("Insira o link da nova foto de perfil");
+        setFotoUsuario(novaFotoUsuario === "" ? fotoUsuario : novaFotoUsuario);
+        // setFoto(!novaFotoUsuario ? foto : novaFotoUsuario);
+    }
+
     return (
-        <div class="usuario">
-          <img src="assets/img/catanacomics.svg" alt="imagem de perfil"/>
-          <div class="texto">
+        <div className="usuario">
+          <img src={fotoUsuario} alt="imagem de perfil" onClick={alteraFoto}/>
+          <div className="texto">
             <span>
-              <strong>catanacomics</strong>
-              <ion-icon name="pencil"></ion-icon>
+              <strong>{nomeUsuario}</strong>
+              <ion-icon name="pencil" onClick={alteraNome} ></ion-icon>
             </span>
           </div>
         </div>
